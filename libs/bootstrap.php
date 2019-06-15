02d3c1ab6337082a201d0c415551aa16ad9989cb
Fonzo <aggadur@gmail.com>
2019-06-15 22:05:37 +0200

    PHPMailer 6.0.7

--- libs/bootstrap.php
+++ libs/bootstrap.php
@@ -7,6 +7,6 @@
  */
 
 // Initialize the auto loader
-@require_once(__DIR__ . '/Fonzo/AutoLoader.php');
+@require_once(__DIR__ . '/PHPMailer/AutoLoader.php');
 \Fonzo\AutoLoader::Register(__DIR__);
 
